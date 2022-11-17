pytorch memory leakage fixed

linear_probing_norm.py
@@ -4,63 +4,74 @@ import evaluate
 from sklearn.linear_model import LogisticRegression
 import torch
 from tqdm import tqdm
+from torch.utils.data import DataLoader
+import numpy as np
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-train = load_dataset('cifar10', split= 'train').with_format("torch").to(device)
-test = load_dataset('cifar10', split='test').with_format("torch").to(device)
+#load CIFAR 10 data
+train = load_dataset('cifar10', split='train').with_format("torch")
+test = load_dataset('cifar10', split='test').with_format("torch")
+train_bn = DataLoader(train, batch_size=100)
+test_bn = DataLoader(test, batch_size=100)
 
-train_imgs = [img for img in train['img']]
-test_imgs = [img for img in test['img']]
-train_labels = [label for label in train['label']]
-test_labels = [label for label in test['label']]
-
-feature_extractor = AutoFeatureExtractor.from_pretrained('microsoft/beit-base-patch16-224-pt22k')
-beit = BeitForMaskedImageModeling.from_pretrained('microsoft/beit-base-patch16-224-pt22k')
-
+feature_extractor = AutoFeatureExtractor.from_pretrained(
+    'microsoft/beit-base-patch16-224-pt22k')
+beit = BeitForMaskedImageModeling.from_pretrained(
+    'microsoft/beit-base-patch16-224-pt22k')
 beit.to(device)
 beit.eval()
 
-n_beit_layers = len(beit.beit.encoder.layer)
+n_beit_layers = len(beit.beit.encoder.layer)  # number of beit layers
 
-def beit_output(data, device='cpu'):
-    features = feature_extractor(data, return_tensors='pt').to(device)
-    output = beit(**features, output_hidden_states = True)
-    return output
 
-train_outputs = beit_output(train_imgs, device=device)
-test_outputs = beit_output(test_imgs, device=device)
-
-train_layers = []
-test_layers = []
-
-def extract_layer(list, outputs):
-    #number of datapoints
-    n = outputs.logits.shape[0]
-
+def beit_output(data_bn, device):
+    outputs = []
+    labels = []
     with torch.no_grad():
-        for i in range(n_beit_layers):
-            layer_output = outputs.hidden_states[i]
-            #hidden state output to normalization layer
-            list.append(beit.beit.encoder.layer[i].layernorm_before(
-                layer_output).reshape(n, -1).cpu().detach().numpy())
-    return list
+        for i, data in enumerate(tqdm(data_bn)):
+            features = feature_extractor(
+                [i for i in data['img']], return_tensors='pt').to(device)
+            output = beit(**features, output_hidden_states=True,
+                          return_dict=True)
 
-extract_layer(train_layers, train_outputs)
-extract_layer(test_layers, test_outputs)
+            layer_output = []
+            for i in range(n_beit_layers):
+                l_output = output.hidden_states[i][:, 1:, :]
+                
+                #apply layernorm
+                if i == n_beit_layers-1:
+                    n_l = beit.layernorm(l_output)
+                else:
+                    n_l = beit.beit.encoder.layer[i+ 1].layernorm_before(l_output)
+
+                layer_output.append(torch.mean(n_l, 1))
+
+            outputs.append(torch.stack(layer_output))
+            labels.append(data['label'])
+
+    return torch.stack(outputs), torch.cat(labels)
+
+
+train_outputs, train_labels = beit_output(train_bn, device)
+test_outputs, test_labels = beit_output(test_bn, device)
+
 
 #linear classification
 model = LogisticRegression()
-
 result = []
 
+emb_dim = train_outputs.shape[-1]
+
 for i in tqdm(range(n_beit_layers)):
-    model.fit(train_layers[i],train_labels)
-    pred = model.predict(test_layers[i])
+    model.fit(train_outputs[:, i, :].cpu().detach(
+    ).numpy().reshape(-1, emb_dim), train_labels)
+    pred = model.predict(test_outputs[:, i, :].cpu(
+    ).detach().numpy().reshape(-1, emb_dim))
 
     metric = evaluate.load('accuracy')
     accuracy = metric.compute(predictions=pred, references=test_labels)
-    
+
     result.append(accuracy.values())
 
 for i in range(n_beit_layers):